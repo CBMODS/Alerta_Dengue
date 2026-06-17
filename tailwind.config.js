180f06aaee97ebd3971da2dc85b3b83ba16cff1a
CBMODS <cristianberrio018@gmail.com>
2026-06-16 20:20:44 -0500

    Actualizar Codigo

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ['./src/**/*.{astro,html,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        bitcount: ['"Bitcount Grid Double"', 'system-ui'],
+      },
+    },
+  },
+  plugins: [],
+}
