d9d4328845eb8af9d655b452c7e0d1c8e58f692b
CBMODS <cristianberrio018@gmail.com>
2026-06-16 20:43:19 -0500

    Test Build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://CBMODS.github.io',
+  base: '/Alerta_Dengue',
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
   }
